da7f7a6688f64874d65e89241b0548265a4309f1
Tiago Silva <tiagobsb84@gmail.com>
2022-01-11 01:28:45 -0300

    Adicionando estrutura backend e frontend

---
+++ backend/src/main/java/com/devtso/tsmovie/TsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devtso.tsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmovieApplication.class, args);
+	}
+
+}
