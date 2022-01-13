af099aca1aa8edf238178be9392a04fe1c3bf785
Tiago Silva <tiagobsb84@gmail.com>
2022-01-13 20:49:39 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import NavBar from "components/NavBar";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return(
+        <h1>Lista</h1>
+    );
+}
+
+export default Listing;
