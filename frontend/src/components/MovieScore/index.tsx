147678b3d7d5b033bc866be8800ae748aa6fd99f
Tiago Silva <tiagobsb84@gmail.com>
2022-01-15 13:00:42 -0300

    Movie Cards

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,20 @@
+import MovieStar from "components/MovieStar";
+
+import "./style.css";
+
+function MovieScore() {
+  const score = 3.5;
+  const count = 13;
+
+  return (
+    <div className="dsmovie-score-container">
+      <p className="dsmovie-score-value">
+        {score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStar />
+      <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+  );
+}
+
+export default MovieScore;
